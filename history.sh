79a767c79e021e90373cd042d42da4454e31c1cb
cduployez <cduployez+githubpro@gmail.com>
2023-07-03 21:34:24 +0200

    Install Sublime-Text

---
+++ history.sh
@@ -0,0 +1,10 @@
+# Configure Git
+git config --global init.defaultBranch main
+git config --global user.email "cduployez+githubpro@gmail.com"
+git config --global user.name "cduployez"
+
+# Install Sublime Text...
+sudo snap install sublime-text --classic
+# ... and its dependencies
+sudo apt install libgl1
+sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
